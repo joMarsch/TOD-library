2178198f15eec932c57a956a132b2fe82990b10a
jonas.marschner@mailbox.org <jonas.marschner@mailbox.org>
2023-11-13 17:56:30 +0100

    Add adding and deleting functionality

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const libraryContainer = document.querySelector(".library-container");
-
-
 function Book(title, author, pages, wasRead) {
     this.title = title;
     this.author = author;
@@ -13,54 +10,113 @@ function Book(title, author, pages, wasRead) {
 }
 
 function Library(container) {
+    let that = this;
     this.library = [];
     this.container = container;
 
     this.addBookToLibrary = function (book) {
         this.library.push(book);
+        this.render();
+    }
+
+    this.removeBookFromLibrary = function (bookIndex) {
+        this.library.splice(bookIndex, 1);
+        this.render();
     }
 
     this.render = function () {
+        this.container.replaceChildren();
         let index = 0;
         this.library.forEach((book) => {
             const card = document.createElement("div");
 
+
+            // Creating children for the card element
             const titleParagraph = document.createElement("p");
             const authorParagraph = document.createElement("p");
             const pagesParagraph = document.createElement("p");
+            const deleteButton = document.createElement("button")
 
+            // Adding Text Content to card children
             titleParagraph.textContent = `Title: ${book.title}`;
             authorParagraph.textContent = `Author: ${book.author}`;
             pagesParagraph.textContent = `Pages: ${book.pages}`;
+            deleteButton.textContent = "Delete";
 
+            
             card.setAttribute("data-index", index);
+
+            // Adding functionality to delete button
+            deleteButton.addEventListener("click", event => {
+                const cardToDelete = event.currentTarget.parentNode;
+                that.removeBookFromLibrary(cardToDelete.dataset.index);
+            });
+
 
             card.appendChild(titleParagraph);
             card.appendChild(authorParagraph);
             card.appendChild(pagesParagraph);
+            card.appendChild(deleteButton)
 
             this.container.appendChild(card);
 
             index++;
 
-             
+
         })
     }
 }
 
+function initializeDialog(library) {
+    const addBookForm = document.querySelector("#add-book-form")
+    const addBookDialog = document.querySelector("dialog");
+    const addBookButton = document.querySelector("dialog + button");
+
+    addBookButton.addEventListener("click", () => {
+        addBookDialog.showModal();
+    });
+
+    addBookForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        let formData = new FormData(addBookForm);
+        let title = formData.get("title");
+        let author = formData.get("author");
+        let pages = Number(formData.get("pages"));
+        let wasRead = (formData.get("wasRead") === "true");
+
+        if(isNaN(pages)) {
+            pages = 0;
+        }
+
+        let book = new Book(title, author, pages, wasRead);
+
+        library.addBookToLibrary(book);
+        console.log(`${title} ${author} ${pages} ${wasRead}`);
+
+
+
+        addBookDialog.close();
+        addBookForm.reset();
+    })
+}
+
 function main() {
+    const libraryContainer = document.querySelector(".library-container");
+    const library = new Library(libraryContainer);
+    
+    initializeDialog(library);
+
     const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 310, false);
     const axiomsEnd = new Book("Axiom's End", "Lindsay Ellis", 384, true);
     const rettungDesImperiums = new Book("Die Rettung des Imperiums", "Isaac Asimov", 560, false);
 
-    const library = new Library(libraryContainer);
     library.addBookToLibrary(theHobbit);
     library.addBookToLibrary(axiomsEnd);
     library.addBookToLibrary(rettungDesImperiums);
     library.addBookToLibrary(theHobbit);
     library.addBookToLibrary(axiomsEnd);
     library.addBookToLibrary(rettungDesImperiums);
-    library.render();
 }
 
 main();
